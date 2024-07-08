aa41c202c3b801885e59e247f2e7411735f766e6
watanabetakuha <taku.neji1126@icloud.com>
2024-07-08 15:31:39 +0900

    レスポンシブデザインの修正

--- src/resources/views/credit.blade.php
+++ src/resources/views/credit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/payment_methods/credit.css') }}" />
+<link rel="stylesheet" href="{{ asset('css/credit.css') }}" />
 @endsection
 
 @section('content')
