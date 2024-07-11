f7d8d7b2034b3493c37f03b4aebfe11e78e5522e
watanabetakuha <taku.neji1126@icloud.com>
2024-07-11 10:37:00 +0900

    出品テストの修正

--- src/tests/Feature/ProductControllerTest.php
+++ src/tests/Feature/ProductControllerTest.php
@@ -6,7 +6,6 @@ use App\Models\Category;
 use App\Models\Product;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
@@ -30,7 +29,7 @@ class ProductControllerTest extends TestCase
         $category = Category::factory()->create(['name' => $categoryName]);
 
         // ダミーの画像ファイルを準備する
-        Storage::fake('public');
+        Storage::fake('s3'); // S3ストレージを使用することを宣言
         $dummyImage = UploadedFile::fake()->image('sample.jpg');
 
         // テスト用リクエストデータの準備
@@ -54,18 +53,18 @@ class ProductControllerTest extends TestCase
         $response->assertStatus(302); // リダイレクトの確認
         $response->assertRedirect(route('home.index')); // リダイレクト先のURLを適宜変更する
 
+        // データベースに商品が保存されたことを確認する
         $this->assertDatabaseHas('products', [
-    'user_id' => $user->id,
-    'status' => 'available',
-    'productName' => 'Sample Product',
-    'brand' => 'Sample Brand',
-    'description' => 'This is a sample product description.',
-    'price' => 1000,
-    'image' => 'images/' . $dummyImage->hashName(),
-]);
+            'user_id' => $user->id,
+            'status' => 'available',
+            'productName' => 'Sample Product',
+            'brand' => 'Sample Brand',
+            'description' => 'This is a sample product description.',
+            'price' => 1000,
+            'image' => 'images/' . $dummyImage->hashName(),
+        ]);
 
-
-        // ストレージに画像が保存されていることを確認する
-        Storage::disk('public')->assertExists('images/' . $dummyImage->hashName());
+        // S3に画像が保存されていることを確認する
+        Storage::disk('s3')->assertExists('images/' . $dummyImage->hashName());
     }
 }
