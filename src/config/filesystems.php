d2331dce828edeb4522045b7c47077b3e4a478ff
watanabetakuha <taku.neji1126@icloud.com>
2024-07-15 22:14:20 +0900

    画像保存コード修正

--- src/config/filesystems.php
+++ src/config/filesystems.php
@@ -30,23 +30,27 @@ return [
 
     'disks' => [
 
-        'default' => env('FILESYSTEM_DRIVER', 'local'),
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+        ],
 
-        'disks' => [
-            'local' => [
-                'driver' => 'local',
-                'root' => storage_path('app/public'),
-                'url' => env('APP_URL').'/storage',
-                'visibility' => 'public',
-            ],
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL').'/storage',
+            'visibility' => 'public',
+        ],
 
-            's3' => [
-                'driver' => 's3',
-                'key' => env('AWS_ACCESS_KEY_ID'),
-                'secret' => env('AWS_SECRET_ACCESS_KEY'),
-                'region' => env('AWS_DEFAULT_REGION'),
-                'bucket' => env('AWS_BUCKET'),
-            ],
+        's3' => [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
 
     ],
--- src/tests/Feature/ProductControllerTest.php
+++ src/tests/Feature/ProductControllerTest.php
@@ -22,17 +22,18 @@ class ProductControllerTest extends TestCase
     public function test_store_product()
 {
     $user = User::factory()->create();
-    $this->actingAs($user);
 
-    // テスト用のカテゴリを作成
+
     $categoryName = 'Sample Category';
     $category = Category::factory()->create(['name' => $categoryName]);
 
-    // テスト用の画像を生成
-    Storage::fake('public');
+    if (App::environment('local')) {
+        Storage::fake('public');
+    } else {
+        Storage::fake('s3');
+    }
     $dummyImage = UploadedFile::fake()->image('sample.jpg');
 
-    // 商品登録用のリクエストデータ
     $requestData = [
         'productName' => 'Sample Product',
         'brand' => 'Sample Brand',
@@ -43,14 +44,14 @@ class ProductControllerTest extends TestCase
         'categories' => [$categoryName],
     ];
 
-    // 商品登録のHTTPリクエストをシミュレート
+    $this->actingAs($user);
+
     $response = $this->post('/product/store', $requestData);
 
-    // リダイレクトの確認
     $response->assertStatus(302);
     $response->assertRedirect(route('home.index'));
 
-    // データベースに商品が正しく保存されたかどうかを確認
+
     $this->assertDatabaseHas('products', [
         'user_id' => $user->id,
         'status' => 'available',
@@ -60,8 +61,11 @@ class ProductControllerTest extends TestCase
         'price' => 1000,
     ]);
 
-    // 画像が正しく保存されたかどうかをストレージで確認
-    Storage::disk('public')->assertExists('images/' . $dummyImage->hashName());
+    if (App::environment('local')) {
+        Storage::disk('public')->assertExists('images/' . $dummyImage->hashName());
+    } else {
+        Storage::disk('s3')->assertExists('images/' . $dummyImage->hashName());
+    }
 }
 
 }
