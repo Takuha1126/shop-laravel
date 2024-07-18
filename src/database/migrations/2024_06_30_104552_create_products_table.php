49e9f6f02c57081b92a2e80eee2e6b66bb241b85
watanabetakuha <taku.neji1126@icloud.com>
2024-07-18 16:57:08 +0900

    コード整理

--- src/database/migrations/2024_06_30_104552_create_products_table.php
+++ src/database/migrations/2024_06_30_104552_create_products_table.php
@@ -16,12 +16,12 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->string('status');
             $table->string('productName');
             $table->string('brand');
             $table->text('description');
             $table->unsignedBigInteger('price');
             $table->string('image');
+            $table->string('status');
             $table->boolean('is_recommended')->default(1);
             $table->timestamps();
         });
