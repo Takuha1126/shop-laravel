01f06727079864f7bef8bc3271664e6d4d136e69
watanabetakuha <taku.neji1126@icloud.com>
2024-07-29 14:59:40 +0900

    コードの整理

--- src/resources/views/admin/verify.blade.php
+++ src/resources/views/admin/verify.blade.php
@@ -5,26 +5,26 @@
 @endsection
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('メールアドレスを確認してください') }}</div>
-                <div class="card-body">
-                    @if (session('resent'))
-                        <div class="alert alert-success" role="alert">
-                            {{ __('新しい確認リンクがあなたのメールアドレスに送信されました。') }}
-                        </div>
-                    @endif
-                    {{ __('続行する前に、確認リンクを含むメールをご確認ください。') }}
-                    {{ __('もしメールが届かない場合は、') }}
-                    <form class="d-inline" method="POST" action="{{ route('admin.verify.resend') }}">
-                        @csrf
-                        <button type="submit" class="button">{{ __('こちらをクリックして別のリンクをリクエストしてください') }}</button>
-                    </form>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('メールアドレスを確認してください') }}</div>
+                    <div class="card-body">
+                        @if (session('resent'))
+                            <div class="alert alert-success" role="alert">
+                                {{ __('新しい確認リンクがあなたのメールアドレスに送信されました。') }}
+                            </div>
+                        @endif
+                        {{ __('続行する前に、確認リンクを含むメールをご確認ください。') }}
+                        {{ __('もしメールが届かない場合は、') }}
+                        <form class="d-inline" method="POST" action="{{ route('admin.verify.resend') }}">
+                            @csrf
+                            <button type="submit" class="button">{{ __('こちらをクリックして別のリンクをリクエストしてください') }}</button>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
