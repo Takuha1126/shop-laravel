0a14615c88f30d714a722fb73d6c667bd112d20a
watanabetakuha <taku.neji1126@icloud.com>
2024-07-30 19:19:22 +0900

    環境構築のエラー修正

--- src/config/database.php
+++ src/config/database.php
@@ -50,7 +50,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'admin'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'Porvej-jyqrub-1zifku'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
