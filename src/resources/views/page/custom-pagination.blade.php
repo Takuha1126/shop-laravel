5a7c7f4629422c1eda9a120dfe92169c79475aec
watanabetakuha <taku.neji1126@icloud.com>
2024-08-02 22:14:43 +0900

    ページネーションの変更

--- src/resources/views/page/custom-pagination.blade.php
+++ src/resources/views/page/custom-pagination.blade.php
@@ -8,33 +8,33 @@
 </head>
 <body>
     <div class="pagination">
-        <ul class="Pagination">
-            @if ($paginator->onFirstPage())
-                <li class="Pagination-Item disabled"><span>&laquo;</span></li>
+    <ul class="Pagination">
+        @if ($paginator->onFirstPage())
+            <li class="Pagination-Item disabled"><span>&laquo;</span></li>
+        @else
+            <li class="Pagination-Item"><a href="{{ $paginator->previousPageUrl() }}" class="Pagination-Item-Link" rel="prev">&laquo;</a></li>
+        @endif
+
+        @php
+            $startPage = max(1, $paginator->currentPage() - 1);
+            $endPage = min($paginator->lastPage(), $paginator->currentPage() + 1);
+        @endphp
+
+        @for ($page = $startPage; $page <= $endPage; $page++)
+            @if ($page == $paginator->currentPage())
+                <li class="Pagination-Item active"><span>{{ $page }}</span></li>
             @else
-                <li class="Pagination-Item"><a href="{{ $paginator->previousPageUrl() }}" class="Pagination-Item-Link" rel="prev">&laquo;</a></li>
+                <li class="Pagination-Item"><a href="{{ $paginator->url($page) }}" class="Pagination-Item-Link">{{ $page }}</a></li>
             @endif
-            @foreach ($elements as $element)
-                @if (is_string($element))
-                    <li class="Pagination-Item disabled"><span>{{ $element }}</span></li>
-                @endif
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <li class="Pagination-Item active"><span>{{ $page }}</span></li>
-                        @else
-                            <li class="Pagination-Item"><a href="{{ $url }}" class="Pagination-Item-Link">{{ $page }}</a></li>
-                        @endif
-                    @endforeach
-                @endif
-            @endforeach
-            @if ($paginator->hasMorePages())
-                <li class="Pagination-Item"><a href="{{ $paginator->nextPageUrl() }}" class="Pagination-Item-Link" rel="next">&raquo;</a></li>
-            @else
-                <li class="Pagination-Item disabled"><span>&raquo;</span></li>
-            @endif
-        </ul>
-    </div>
+        @endfor
+
+        @if ($paginator->hasMorePages())
+            <li class="Pagination-Item"><a href="{{ $paginator->nextPageUrl() }}" class="Pagination-Item-Link" rel="next">&raquo;</a></li>
+        @else
+            <li class="Pagination-Item disabled"><span>&raquo;</span></li>
+        @endif
+    </ul>
+</div>
 </body>
 </html>
 
