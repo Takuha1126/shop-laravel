310794a79de1192e4c8b26a4f1320fa95a431cd4
watanabetakuha <taku.neji1126@icloud.com>
2024-08-09 15:57:45 +0900

    バリデーションのメッセージ修正

--- src/app/Http/Requests/ProductRequest.php
+++ src/app/Http/Requests/ProductRequest.php
@@ -53,7 +53,7 @@ class ProductRequest extends FormRequest
             'brand.max' => 'ブランド名は30文字以内で入力してください。',
             'description.required' => '商品の説明を入力してください。',
             'description.string' => '商品の説明は文字列で入力してください。',
-            'description.max' => '商品の説明は255文字以下で入力してください。',
+            'description.max' => '商品の説明は255文字以内で入力してください。',
             'price.required' => '販売価格を入力してください。',
             'price.numeric' => '販売価格は数値で入力してください。',
             'price.min' => '販売価格は0以上で入力してください。',
